2ae58db0ecb98633633f38d07567a933d50adb46
Brianna Giunta <brig_oowee@Briannas-MacBook-Pro.local>
2021-05-26 16:08:45 -0400

    deploy

--- application.py
+++ application.py
@@ -293,6 +293,6 @@ def handleMessage(msg):
     return None
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    # port = int(os.environ.get('PORT', 5000))
     # app.run(host='0.0.0.0', port=port, debug=True)
     socket_io.run(app)
